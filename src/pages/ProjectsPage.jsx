58c730ecae1270bdd0594cb6054368cad4a72d84
taha-elbadaoui <taha.elbadaoui.pro@gmail.com>
2026-02-19 22:26:03 +0000

    Changed header and footer

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,1 +1,9 @@
-//This is the projectspage of the application
+import React from 'react';
+
+export default function ProjectsPage() {
+  return (
+    <div className="pt-32 min-h-screen">
+      <h1 className="text-3xl font-bold text-center">Page Projets</h1>
+    </div>
+  );
+}
